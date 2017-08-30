a4a7a3fa66376f88d9e9ed7e3533ca4a7d7f8aa6
Axel <axl89@users.noreply.github.com>
2017-08-30 22:50:20 +0200

    Update timetable.cs
    Transformed c1 to a bidimensional array (now named buttons).

--- timetable.cs
+++ timetable.cs
@@ -1,26 +1,49 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class timetable : MonoBehaviour {
+public class timetable : MonoBehaviour
+{
 
     public GameObject popUp;
     public GameObject inputField;
     public GameObject inputFieldtB;
     public GameObject inputFieldcB;
+    public int weekDays;
+    public int slots;
+
     protected bool isTasking;
-    protected string c1; //Need 49 copies
-  
+    protected string[][] buttons; //Need 49 copies (7 days x 7 slots)
 
 
+    /*
+     * Initialize the buttons bidimensional array given weekDays and slots
+     */
+    protected void initializeButtons()
+    {
+        for(int day = 0; day < weekDays; day++)
+        {
+            for (int slot = 0; slot < slots; slot++)
+            {
+                buttons[day,slot] = "";
+            }
+            
+        }
+    }
     // Use this for initialization
-    void Start() {
-        
+    void Start()
+    {
+        weekDays = 7; //7 days a week
+        slots = 7; //7 slots per day
+        buttons = new string[weekDays,slots]; // Makes a bidimensional array of 7x7
+
+        initializeButtons();
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         if (isTasking == true)
         {
             GameObject.FindWithTag("gap").GetComponent<Image>().color = new Color(1f, 1f, 1f, 0.5f);
